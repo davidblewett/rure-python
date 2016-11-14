0e8c78a2c8f9c9d17a9921bff8a9cc5a7166c9e8
Matt DeBoard <matt.deboard@crowdstrike.com>
2016-11-14 14:26:48 -0600

    Add `message` property to `RegexError` class.
    `BaseException.message` was deprecated in Python 3.

--- rure/exceptions.py
+++ rure/exceptions.py
@@ -1,5 +1,10 @@
+from __future__ import unicode_literals
+
+
 class RegexError(Exception):
-    pass
+    def __init__(self, message, *args):
+        super(RegexError, self).__init__(message, *args)
+        self.message = str(message)
 
 
 class RegexSyntaxError(RegexError):
