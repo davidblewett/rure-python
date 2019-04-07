d0e5ee26a5ddc0bf8c815079faeb15d7777015ba
David Blewett <davidblewett@simple.com>
2019-04-07 15:23:45 -0400

    Add version classifiers.

--- setup.py
+++ setup.py
@@ -51,5 +51,9 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Text Processing :: Indexing']
 )
